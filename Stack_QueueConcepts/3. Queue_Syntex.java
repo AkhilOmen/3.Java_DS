af2d735b542028280779f350d6bb53cc8cb8d3e3
AkhilOmen <akhilbv333@gmail.com>
2021-11-04 13:49:15 +0530

    Added Upto Queue

--- Stack_QueueConcepts/3. Queue_Syntex.java
+++ Stack_QueueConcepts/3. Queue_Syntex.java
@@ -2,5 +2,10 @@
 Queue<Integer> q = new Queue<>();
 
 // Functionalities
-Refere to the code..! 
-Normal Queue and Dynamic Queue...
+// Refere to the code..! 
+// Normal Queue and Dynamic Queue...
+
+.add();
+.peek();
+.remove();
+.size();
