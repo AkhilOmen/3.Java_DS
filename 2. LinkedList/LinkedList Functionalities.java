50ee91692baffd1e203d2dceb230266a70895f8a
AkhilOmen <akhilbv333@gmail.com>
2021-11-07 06:55:58 +0530

    Added Upto LinkedList Functionalities

---
+++ 2. LinkedList/LinkedList Functionalities.java
@@ -0,0 +1,16 @@
+
+
+// .........................add
+.addLast(val);
+.addFirst(val);
+.addAt(idx, val);
+
+// ................................get
+.getLast();
+.getFirst();
+.getAt(idx);
+
+// ..............................remove
+.removeLast();
+.removeFirst();
+.removeAt(idx);
