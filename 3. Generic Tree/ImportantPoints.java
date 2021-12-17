3dfed1e461ad2b597816e79ac3d4767128d8d71d
AkhilOmen <akhilbv333@gmail.com>
2021-12-18 00:11:27 +0530

    Added upto some Algo in Generic Tree

---
+++ 3. Generic Tree/ImportantPoints.java
@@ -0,0 +1,16 @@
+// Algo in Generic Tree
+
+// 1. Level Order Of Generic Tree -> Use 
+//             Deque<Node> mq = new Deque<>();
+
+// 2. Level Order Linewise -> Use
+//             Deque<Node> mq = new Deque<>();
+//             Deque<Node> cq = new Deque<>();
+            
+// 3. Level Order Linewise Zig Zag -> Use
+//                 Stack<Node> ms = new Stack<>();
+//                 Stack<Node> cs = new Stack<>();
+//                 level
+
+
+
