0f44ec055e1218e8f9b3d69100dd44630cd541d3
AkhilOmen <akhilbv333@gmail.com>
2021-12-18 14:20:38 +0530

    Added Upto Linearize Generic Tree Algo

--- 3. Generic Tree/ImportantPoints.java
+++ 3. Generic Tree/ImportantPoints.java
@@ -12,5 +12,29 @@
 //                 Stack<Node> cs = new Stack<>();
 //                 level
 
+// 4. Linearize A Generic Tree
+    // Awesome Algorithm---
+// public static void linearize(Node root){
+    
+//     for( Node child : root.children){
+//         linearize(child);
+//     }
+    
+//     for(int i = root.children.size() - 1; i >= 1 ; i--){
+        
+//         Node child1 = root.children.remove(i);
+        
+//         //here we are finding out the tail of n-1th node of the root child..
+//         Node temp = root.children.get(i-1);
+//         while(temp.children.size() != 0 ){
+//             temp = temp.children.get(0);
+//         }
+        
+//         temp.children.add(child1);
+//     }
+    
+// }
 
+
+// 5.
 
